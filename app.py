90a1d550d479e8d5711a9a56b45eb31c9d215b01
Sarang Kumar <sarangkumar1578@gmail.com>
2023-02-25 12:46:31 +0000

    add flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+  # print("i am inside the main")
+  app.run(host='0.0.0.0', debug=True)
